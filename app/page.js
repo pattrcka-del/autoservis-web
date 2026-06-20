62858fd917e35f8a7c64a39a177e14ce0774b2e7
pattrcka-del <pat.trcka@icloud.com>
2026-06-21 00:34:32 +0200

    Update heading text in Home component

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <main style={{background: "black", color: "white", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
-      <h1>AUTO SERVIS WEB FUNGUJE 🔥</h1>
+    <main style={{
+      background: "black",
+      color: "white",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <h1>WEB FUNGUJE 🔥</h1>
     </main>
   );
 }
